53f30b6b94e978f2ea1f01e92dd39e21ecdabef0
林方晴 <linfangqing@linfangqingdeMacBook-Air-2.local>
2024-07-22 16:37:15 +0800

    write static stite generator

---
+++ insta485generator/__main__.py
@@ -0,0 +1,13 @@
+"""Build static HTML site from directory of HTML templates and plain files."""
+
+
+@click.command()
+@click.argument("input_dir", nargs=1, type=click.Path(exists=True))
+def main(input_dir):
+    input_dir = pathlib.Path(input_dir)
+    print(f"DEBUG input_dir={input_dir}")
+
+if __name__ == "__main__":
+    main()
+
+
